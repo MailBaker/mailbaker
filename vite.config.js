252f1efab1beecead1b15636c49b811076b7b665
Šarūnas <39726634+sarunaslite@users.noreply.github.com>
2024-02-02 00:45:45 +0200

    tailwind and directory update

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,28 @@ import juice from '@vituum/vite-plugin-juice'
 import send from '@vituum/vite-plugin-send'
 
 export default {
+    build: {
+        rollupOptions: {
+            input: [
+                './src/emails/*.{json,latte,twig,liquid,njk,hbs,pug,html}',
+                './src/templates/**/*.{json,latte,twig,liquid,njk,hbs,pug,html}',
+                '!./src/templates/**/*.{latte,twig,liquid,njk,hbs,pug,html}.json'
+            ],
+        },
+    },
     plugins: [
-        vituum(),
+        vituum({
+            pages: {
+                dir: './src/templates'
+            }
+        }),
         tailwindcss(),
         posthtml({
             root: './src/emails'
         }),
-        juice(),
+        juice({
+            paths: ['src/templates']
+        }),
         send()
     ]
 }
